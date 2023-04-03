6c9983c60f72e4b8827fa110303df026aec21e68
Joseph Muto <jmuto2@gmail.com>
2023-04-03 17:31:02 -0400

    DS-5017-v3 add a setter for production just in case

--- src/Traits/HasEnvironmentsTrait.php
+++ src/Traits/HasEnvironmentsTrait.php
@@ -5,10 +5,21 @@ namespace AllDigitalRewards\NeoCurrency\Traits;
 trait HasEnvironmentsTrait
 {
     private string $baseUrl = 'https://redeem.yourdigitalreward.com';
+    private bool $isProduction = false;
+
+    public function isProduction(): bool
+    {
+        return $this->isProduction;
+    }
+
+    public function setIsProduction(bool $isProduction): void
+    {
+        $this->isProduction = $isProduction;
+    }
 
     public function getBaseUrl(): string
     {
-        return getenv('ENVIRONMENT') === 'PRODUCTION'
+        return getenv('ENVIRONMENT') === 'PRODUCTION' || $this->isProduction()
             ? $this->baseUrl . '/api'
             : $this->baseUrl . '/api/sandbox';
     }
